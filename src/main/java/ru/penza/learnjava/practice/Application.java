d03d38b1def394e1b9923c257d9b307613b5261a
manfromsky <rempenza@mail.ru>
2018-01-04 20:05:09 +0300

    solved the problem with time zones

--- src/main/java/ru/penza/learnjava/practice/Application.java
+++ src/main/java/ru/penza/learnjava/practice/Application.java
@@ -2,11 +2,13 @@ package ru.penza.learnjava.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Mihail on 26.12.2017.
  */
+@SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
